ed0d4e3c757525e620468c7b73da79e4cd512bb8
Toby Scott <hi@tobyscott.dev>
2023-06-07 14:31:09 +1000

    add comment about trying to use CUI in mf

--- microfrontend-one/src/components/App.jsx
+++ microfrontend-one/src/components/App.jsx
@@ -1,11 +1,14 @@
 // This is the microfrontend entry point.
 
 import * as React from 'react';
+// import { Input } from '@chakra-ui/react';
 
 export default function App({ onChange }) {
   return (
     <div>
       <h1>MICROFRONTEND ONE (REMOTE)</h1>
+      {/* <Input onChange={onChange} type="text" placeholder="Enter your name" /> */}
+      {/* Using Chakra UI in the microfrontend is causing "Invalid hook call" and "Cannot read properties of null (reading 'useContext')" errors. */}
       <input onChange={onChange} type="text" placeholder="Enter your name" />
     </div>
   );
