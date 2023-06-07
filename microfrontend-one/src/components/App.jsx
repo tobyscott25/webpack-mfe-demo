71fbe0ceed0006e61c8f960a573189880a1186c5
Toby Scott <hi@tobyscott.dev>
2023-06-07 13:40:30 +1000

    rename container app

--- microfrontend-one/src/components/App.jsx
+++ microfrontend-one/src/components/App.jsx
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App({ onChange }) {
   return (
     <div className="MicroApp">
-      <h1>MICROFRONTEND (REMOTE)</h1>
+      <h1>MICROFRONTEND ONE (REMOTE)</h1>
       <input onChange={onChange} type="text" placeholder="Enter your name" />
     </div>
   );
