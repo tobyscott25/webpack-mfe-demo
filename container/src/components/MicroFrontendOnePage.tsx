eae180ebc6dd1b60a543bb37e1033435303e3343
Toby Scott <hi@tobyscott.dev>
2023-06-28 18:15:27 +1000

    add lazy to component names

--- container/src/components/MicroFrontendOnePage.tsx
+++ container/src/components/MicroFrontendOnePage.tsx
@@ -2,7 +2,7 @@ import { lazy, Suspense, useState, FunctionComponent } from "react";
 import { Box, Text } from '@chakra-ui/react';
 import "../assets/styles.css";
 
-const MicrofrontendOne = lazy(() => import("microfrontend1/app"));
+const MicroFrontendOneLazy = lazy(() => import("microfrontend1/app"));
 
 export const MicroFrontendOnePage: FunctionComponent = (): JSX.Element => {
   const [text, setText] = useState<string>('');
@@ -12,7 +12,7 @@ export const MicroFrontendOnePage: FunctionComponent = (): JSX.Element => {
       { text && <Text>Evidence of shared state: Your text is: {text}</Text> }
       <Box border={'1px solid black'} shadow={'lg'} rounded={'lg'} mx={'300px'} my={5} className="App">
         <Suspense fallback={<Text>Loading...</Text>}>
-          <MicrofrontendOne onChange={(event) => setText(event.target.value)} />
+          <MicroFrontendOneLazy onChange={(event) => setText(event.target.value)} />
         </Suspense>
       </Box>
     </Box>
--- container/src/components/MicroFrontendTwoPage.tsx
+++ container/src/components/MicroFrontendTwoPage.tsx
@@ -2,7 +2,7 @@ import { lazy, Suspense, useState, FunctionComponent } from "react";
 import { Box, Text } from '@chakra-ui/react';
 import "../assets/styles.css";
 
-const MicrofrontendTwo = lazy(() => import("microfrontend2/app"));
+const MicroFrontendTwoLazy = lazy(() => import("microfrontend2/app"));
 
 export const MicroFrontendTwoPage: FunctionComponent = (): JSX.Element => {
   const [text, setText] = useState<string>('');
@@ -12,7 +12,7 @@ export const MicroFrontendTwoPage: FunctionComponent = (): JSX.Element => {
       { text && <Text>Evidence of shared state: Your text is: {text}</Text> }
       <Box border={'1px solid black'} shadow={'lg'} rounded={'lg'} mx={'300px'} my={5} className="App">
         <Suspense fallback={<Text>Loading...</Text>}>
-          <MicrofrontendTwo onChange={(event) => setText(event.target.value)} />
+          <MicroFrontendTwoLazy onChange={(event) => setText(event.target.value)} />
         </Suspense>
       </Box>
     </Box>
